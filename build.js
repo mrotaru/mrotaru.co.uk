0058fbf7de98ba883673485dfe3c7d8bf2f3105d
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-04-11 08:13:23 +0100

    use handlebars templates

--- build.js
+++ build.js
@@ -10,18 +10,23 @@ const rimraf = utils.promisify(require("rimraf"));
 
 const baseUrl = process.env.BASE_URL || "http://localhost:8080";
 const tidyOptions = {
-  doctype: "omit",
+  "doctype": "omit",
   "error-file": "/dev/null",
   "indent-spaces": 2,
-  indent: "yes",
+  "indent": "yes",
   "omit-optional-tags": false,
   "output-xhtml": true,
   "show-body-only": true,
   "show-info": false,
   "show-warnings": false,
   "tidy-mark": false,
-  wrap: 120
+  "wrap": 120
 };
+
+const compileTemplates = async () => ({
+  "page": handlebars.compile(await fs.readFile("./template-page.html", { encoding: "utf8" })),
+  "blog-post": handlebars.compile(await fs.readFile("./template-blog-post.html", { encoding: "utf8" })),
+})
 
 const markdownToHtml = (markdownText, { markedOptions = {} } = {}) => {
   marked.setOptions(Object.assign({ xhtml: true }, markedOptions));
@@ -45,7 +50,8 @@ const buildDir = async ({
   destination,
   parent = "",
   removeIfExisting = true,
-  createDestination = true
+  createDestination = true,
+  templates,
 }) => {
   console.log(`buid: ${source} 🠆 ${destination} ...`);
   const entries = await fs.readdir(source);
@@ -74,33 +80,39 @@ const buildDir = async ({
         destination,
         parent: join(parent, entry),
         removeIfExisting: false,
-        createDestination: false
+        createDestination: false,
+        templates,
       });
     } else {
-      // TODO: if no md file in 'index' folder, build a page with links to other pages in same folder
       await buildDir({
         source: joinedSource,
         destination: joinedDestination,
-        parent: join(parent, entry)
+        parent: join(parent, entry),
+        templates,
       });
     }
   }
-  for (const entry of files) {
+  let meta = {}
+  if (files.includes("meta.json")) {
+    const json = await fs.readFile(join(source, "meta.json"), { encoding: "utf8" });
+    meta = JSON.parse(json)
+  }
+  for (const entry of files.filter(file => file !== "meta.json")) {
     const joined = join(source, entry);
     const ext = extname(entry);
     if (ext === ".md") {
       const parentBaseUrl = url.resolve(baseUrl, parent);
       const markdown = await fs.readFile(joined, { encoding: "utf8" });
-      const html = markdownToHtml(markdown, {
-        markedOptions: { baseUrl: `${parentBaseUrl}/` }
+      const markedOptions = { baseUrl: `${parentBaseUrl}/` }
+      const html = templates[meta.template]({
+        content: markdownToHtml(markdown, { markedOptions }),
+        ...meta,
       });
       await fs.writeFile(
         join(destination, `${basename(entry, ".md")}.html`),
         html,
         { encoding: "utf8" }
       );
-    } else if (entry === "meta.json") {
-      // console.log(require(absolute));
     } else {
       await fs.copyFile(joined, join(destination, entry));
     }
@@ -108,11 +120,9 @@ const buildDir = async ({
 };
 
 const build = async ({ source, destination }) => {
-  const templateString = await fs.readFile("./template.html", {
-    encoding: "utf8"
-  });
-  const template = handlebars.compile(templateString);
-  await buildDir({ source, destination }).catch(err =>
+  const templates = await compileTemplates()
+  console.log(templates)
+  await buildDir({ source, destination, templates }).catch(err =>
     console.log(`build dir ${source} failed:`, err)
   );
 };
