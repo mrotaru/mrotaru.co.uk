f3e64bb2aff6def788bb793f96bb7ca94a7dbbfe
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-04-13 20:27:55 +0100

    build folder indexes

---
+++ build-utils.js
@@ -0,0 +1,72 @@
+const fs = require("fs").promises;
+const marked = require("marked");
+const handlebars = require("handlebars");
+const ent = require("ent");
+const tidy = require("tidy-html5");
+
+const estat = async path => {
+  try {
+    return await fs.lstat(path);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      return null;
+    } else {
+      throw err;
+    }
+  }
+};
+
+const readFile = async (path, options) =>
+  await fs.readFile(path, { encoding: "utf8", ...options });
+
+const readJson = async file => {
+  const stat = await estat(file);
+  if (stat) {
+    const json = await readFile(file);
+    return JSON.parse(json);
+  }
+};
+
+const compileTemplates = async () => {
+  handlebars.registerPartial("head", await readFile("./templates/_head.html"));
+  handlebars.registerPartial(
+    "footer",
+    await readFile("./templates/_footer.html")
+  );
+  const r1 = await fs.readFile("./templates/page.html");
+  return {
+    "page": handlebars.compile(await readFile("./templates/page.html")),
+    "blog-post": handlebars.compile(await readFile("./templates/blog-post.html")),
+    "folder-index": handlebars.compile(await readFile("./templates/folder-index.html")),
+  };
+};
+
+const markdownToHtml = (markdownText, { markedOptions = {} } = {}) => {
+  const tidyOptions = {
+    "doctype": "omit",
+    "error-file": "/dev/null",
+    "indent-spaces": 2,
+    "indent": "yes",
+    "omit-optional-tags": false,
+    "output-xhtml": true,
+    "show-body-only": true,
+    "show-info": false,
+    "show-warnings": false,
+    "tidy-mark": false,
+    "wrap": 120
+  };
+  marked.setOptions(Object.assign({ xhtml: true }, markedOptions));
+  const renderer = new marked.Renderer();
+  const htmlFromMarkdown = marked(markdownText, { renderer });
+  const withDecodeHtmlEntities = ent.decode(htmlFromMarkdown);
+  const tidiedHtml = tidy.tidy_html5(withDecodeHtmlEntities, tidyOptions);
+  return tidiedHtml;
+};
+
+exports.utils = {
+  readFile,
+  readJson,
+  compileTemplates,
+  estat,
+  markdownToHtml
+};
--- build.js
+++ build.js
@@ -1,61 +1,21 @@
 const utils = require("util");
 const fs = require("fs").promises;
-const marked = require("marked");
-const handlebars = require("handlebars");
-const ent = require("ent");
-const tidy = require("tidy-html5");
-const { normalize, join, resolve, extname, basename } = require("path");
+const { normalize, join, resolve, extname, basename, sep } = require("path");
+const assert = require('assert')
 const url = require("url");
+const {
+  readFile,
+  readJson,
+  compileTemplates,
+  estat,
+  markdownToHtml
+} = require('./build-utils').utils
 const ncp = utils.promisify(require("ncp").ncp);
 const rimraf = utils.promisify(require("rimraf"));
 
-const readFile = async (path, options) => await fs.readFile(path, { encoding: "utf8", ...options })
+let indexedMetas = {}
+
 const baseUrl = process.env.BASE_URL || "http://localhost:8080";
-const tidyOptions = {
-  "doctype": "omit",
-  "error-file": "/dev/null",
-  "indent-spaces": 2,
-  "indent": "yes",
-  "omit-optional-tags": false,
-  "output-xhtml": true,
-  "show-body-only": true,
-  "show-info": false,
-  "show-warnings": false,
-  "tidy-mark": false,
-  "wrap": 120
-};
-
-const compileTemplates = async () => {
-  handlebars.registerPartial('head', await readFile('./templates/_head.html'))
-  handlebars.registerPartial('footer', await readFile('./templates/_footer.html'))
-  const r1= await fs.readFile("./templates/page.html")
-  return {
-    "page": handlebars.compile(await readFile("./templates/page.html")),
-    "blog-post": handlebars.compile(await readFile("./templates/blog-post.html")),
-  }
-};
-
-const markdownToHtml = (markdownText, { markedOptions = {} } = {}) => {
-  marked.setOptions(Object.assign({ xhtml: true }, markedOptions));
-  const renderer = new marked.Renderer();
-  const htmlFromMarkdown = marked(markdownText, { renderer });
-  const withDecodeHtmlEntities = ent.decode(htmlFromMarkdown);
-  const tidiedHtml = tidy.tidy_html5(withDecodeHtmlEntities, tidyOptions);
-  return tidiedHtml;
-};
-
-const estat = async path => {
-  try {
-    return await fs.lstat(path);
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      return null;
-    } else {
-      throw err;
-    }
-  }
-};
-
 const buildDir = async ({
   source,
   destination,
@@ -75,6 +35,7 @@ const buildDir = async ({
   if (!stat && createDestination) {
     await fs.mkdir(destination);
   }
+
   const dirs = [];
   const files = [];
   for (let entry of entries) {
@@ -82,6 +43,22 @@ const buildDir = async ({
     stat.isDirectory() && dirs.push(entry);
     stat.isFile() && files.push(entry);
   }
+
+  indexedMetas = []
+  for (const entry of dirs.filter(dir => dir !== 'index')) {
+    console.log(source, entry)
+    const indexedDir = join(source, entry);
+    const meta = await readJson(join(indexedDir, "meta.json"))
+    const path = indexedDir.split(sep).slice(1).join(sep)
+    indexedMetas.push({ ...meta, path, url: url.resolve(baseUrl, path) })
+  }
+
+  let meta = {};
+  if (files.includes("meta.json")) {
+    const json = await readFile(join(source, "meta.json"))
+    meta = { ...JSON.parse(json), index: indexedMetas }
+  }
+
   for (const entry of dirs) {
     const joinedSource = join(source, entry);
     const joinedDestination = join(destination, entry);
@@ -103,11 +80,6 @@ const buildDir = async ({
       });
     }
   }
-  let meta = {};
-  if (files.includes("meta.json")) {
-    const json = await readFile(join(source, "meta.json"))
-    meta = JSON.parse(json);
-  }
   for (const entry of files.filter(file => file !== "meta.json")) {
     const joined = join(source, entry);
     const ext = extname(entry);
@@ -115,6 +87,8 @@ const buildDir = async ({
       const parentBaseUrl = url.resolve(baseUrl, parent);
       const markdown = await readFile(joined);
       const markedOptions = { baseUrl: `${parentBaseUrl}/` };
+      assert(templates.hasOwnProperty(meta.template), `No such template: ${meta.template}`)
+      console.log(joined, meta)
       const html = templates[meta.template]({
         content: markdownToHtml(markdown, { markedOptions }),
         meta,
